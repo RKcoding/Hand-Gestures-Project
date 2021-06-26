5c493a240748b349c08d9b85469724ce912db787
RKcoding <73450077+RKcoding@users.noreply.github.com>
2021-06-25 22:59:39 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -21,3 +21,41 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log("Model Loaded!!!");
 }
+
+function CheckSnapshot() {
+    CapturedImage = document.getElementById("captured_image");
+    classifier.classify(CapturedImage, GotResult);
+}
+
+function GotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("hand_gesture_name").innerHTML = results[0].label;
+        Prediction = results[0].label;
+        speak();
+        if (results[0].label == "Amazing") {
+            document.getElementById("hand_gesture_icon").innerHTML = "&#128076;";
+        }
+        if (results[0].label == "Good") {
+            document.getElementById("hand_gesture_icon").innerHTML = "&#128077;";
+        }
+        if (results[0].label == "Bad") {
+            document.getElementById("hand_gesture_icon").innerHTML = "&#128078;";
+        }
+        if (results[0].label == "Left") {
+            document.getElementById("hand_gesture_icon").innerHTML = "&#128072;";
+        }
+        if (results[0].label == "Power") {
+            document.getElementById("hand_gesture_icon").innerHTML = "&#9994;";
+        }
+    }
+}
+
+function speak() {
+    var SpeechSynth = window.speechSynthesis;
+    SpeakData = "The results are " + Prediction;
+    var utterThis = new SpeechSynthesisUtterance(SpeakData);
+    SpeechSynth.speak(utterThis);
+}
